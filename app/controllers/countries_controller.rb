da375503e8d86b1b9b11bed89302d9078f49fc7b
jgumerove <josh.gumerove@gmail.com>
2021-11-22 12:58:41 -0500

    country controller show action

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -3,4 +3,9 @@ class CountriesController < ApplicationController
         countries = Country.all
         render json: countries
     end
+
+    def show
+        country = Country.find(params[:id])
+        render json: country
+    end
 end
