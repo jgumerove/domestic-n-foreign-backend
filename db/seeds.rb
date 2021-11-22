d865c1334fe654eb348d1aab42695732a9434b84
jgumerove <josh.gumerove@gmail.com>
2021-11-22 17:22:50 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,16 @@ long: 55.7558, lat: 37.6173)
 #coordinates for Beijing
 china = Country.create(name: "China", flag: "https://cdn.britannica.com/90/7490-004-BAD4AA72/Flag-China.jpg",
 long: 39.9042, lat: 116.4074)
+
+#coordinates for Canberra
+australia = Country.create(name: "Australia", flag: "https://cdn.britannica.com/78/6078-004-77AF7322/Flag-Australia.jpg",
+long: -35.2809, lat: 149.1300)
+
+#coordinates for Ottawa
+canada = Country.create(name: "Canada", flag: "https://www.worldatlas.com/img/flag/ca-flag.jpg",
+long: 45.4215, lat: -75.6972)
+
+#coordinates for Paris
+france = Country.create(name: "France", flag: "https://cdn.britannica.com/82/682-050-8AA3D6A6/Flag-France.jpg",
+long: 48.8566, lat: 2.3522)
+
