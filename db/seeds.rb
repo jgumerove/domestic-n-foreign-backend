3f24f5951083e7bddaaa47dffc7f6c8836c940ec
jgumerove <josh.gumerove@gmail.com>
2021-11-22 20:32:49 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 #coordinate notes: all southern and western values are negative
 
 #coordinates for Washington DC
-united_states = Country.create(name: "United States", flag: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png",
+united_states = Country.create(name: "United States", flag: "https://cdn.britannica.com/33/4833-004-828A9A84/Flag-United-States-of-America.jpg",
 long: 38.9072, lat: -77.0369)
 
 #coordinates for Moscow
@@ -17,10 +17,69 @@ australia = Country.create(name: "Australia", flag: "https://cdn.britannica.com/
 long: -35.2809, lat: 149.1300)
 
 #coordinates for Ottawa
-canada = Country.create(name: "Canada", flag: "https://www.worldatlas.com/img/flag/ca-flag.jpg",
+canada = Country.create(name: "Canada", flag: "https://cdn.britannica.com/68/7068-004-7848FEB4/Flag-Canada.jpg",
 long: 45.4215, lat: -75.6972)
 
 #coordinates for Paris
 france = Country.create(name: "France", flag: "https://cdn.britannica.com/82/682-050-8AA3D6A6/Flag-France.jpg",
 long: 48.8566, lat: 2.3522)
+
+#coordinates for Berlin
+germany = Country.create(name: "Germany", flag: "https://cdn.britannica.com/97/897-004-232BDF01/Flag-Germany.jpg",
+long: 52.5200, lat: 13.4050)
+
+#coordinates for Athens 
+greece = Country.create(name: "Greece", flag: "https://cdn.britannica.com/49/1049-004-AE4BAD3E/Flag-Greece.jpg",
+long: 37.9838, lat: 23.7275)
+
+#coordinates for New Delhi
+india = Country.create(name: "India", flag: "https://cdn.britannica.com/97/1597-004-05816F4E/Flag-India.jpg",
+long: 28.6139, lat: 77.2090)
+
+#coordinates for Tehran
+iran = Country.create(name: "Iran", flag: "https://cdn.britannica.com/22/1722-004-EAD033D8/Flag-Iran.jpg",
+long: 35.6892, lat: 51.3890)
+
+#coordinates for Jerusalem
+israel = Country.create(name: "Israel", flag: "https://cdn.britannica.com/53/1753-004-03582EDA/Flag-Israel.jpg",
+long: 31.7683, lat: 35.2137)
+
+#coordinates for Rome 
+italy = Country.create(name: "Italy", flag: "https://cdn.britannica.com/59/1759-004-F4175463/Flag-Italy.jpg",
+long: 41.9028, lat: 12.4964)
+
+#coordinates for Tokyo
+japan = Country.create(name: "Japan", flag: "https://cdn.britannica.com/91/1791-050-270C9B3E/Flag-Japan.jpg",
+long: 35.6762, lat: 139.6503)
+
+#coordinates for Amsterdam
+netherlands = Country.create(name: "The Netherlands", flag: "https://cdn.britannica.com/82/2982-004-84A2A71E/flag-prototype-Netherlands-countries-European-flags.jpg",
+long: 52.3676, lat: 4.9041)
+
+#coordinates for Islamabad
+pakistan = Country.create(name: "Pakistan", flag: "https://cdn.britannica.com/w:400,h:235,c:crop/46/3346-050-F71DF512/flag-symbolism-Pakistan-design-Islamic.jpg",
+long: 33.6844, lat: 73.0479)
+
+#coordinates for Riyadh
+saudi_arabia = Country.create(name: "Saudi Arabia", flag: "https://cdn.britannica.com/79/5779-004-DC479508/Flag-Saudi-Arabia.jpg",
+long: 24.7136, lat: 46.6753)
+
+#coordinates for Singapore
+singapore = Country.create(name: "Singapore", flag: "https://cdn.britannica.com/w:400,h:235,c:crop/36/4036-050-37052A78/Flag-Singapore.jpg",
+long: 1.3521, lat: 103.8198)
+
+#coordinates for Ankara
+turkey = Country.create(name: "Turkey", flag: "https://cdn.britannica.com/w:400,h:235,c:crop/82/4782-050-8129909C/Flag-Turkey.jpg",
+long: 39.9334, lat: 32.8597)
+
+#coordinates for Abu Dhabi
+united_arab_emirates = Country.create(name: "United Arab Emirates", flag: "https://cdn.britannica.com/82/5782-004-BD7056FF/Flag-United-Arab-Emirates.jpg",
+long: 24.4539, lat: 54.3773)
+
+#coordinates for London
+united_kingdom = Country.create(name: "United Kingdom", flag: "https://cdn.britannica.com/w:400,h:235,c:crop/25/4825-050-977D8C5E/Flag-United-Kingdom.jpg",
+long: 51.5072, lat: -0.1276)
+
+
+
 
