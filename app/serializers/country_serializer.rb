04ccfffbf818eaaae594dd442def406dfcf0c321
jgumerove <josh.gumerove@gmail.com>
2021-11-23 17:04:47 -0500

    add base count to json

--- app/serializers/country_serializer.rb
+++ app/serializers/country_serializer.rb
@@ -1,4 +1,8 @@
 class CountrySerializer < ActiveModel::Serializer
-  attributes :id, :name, :flag, :long, :lat
+  attributes :id, :name, :flag, :long, :lat, :base_count
+
+  def base_count
+    self.object.places.count
+  end
 
 end
