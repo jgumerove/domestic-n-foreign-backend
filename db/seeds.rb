d126c1d5b0dc348dfdca96e158785b20e7899457
jgumerove <josh.gumerove@gmail.com>
2021-11-25 14:17:02 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,32 @@
 #coordinate notes: all southern and western values are negative
 #adding test data for bases
 
-#coordinates for Washington DC
+###coordinates for Washington DC
 united_states = Country.create(name: "United States", flag: "https://cdn.britannica.com/33/4833-004-828A9A84/Flag-United-States-of-America.jpg",
 long: 38.9072, lat: -77.0369)
-united_states.places.create(base_long: 34.0522, base_lat: -118.2437, location: "Los Angeles", base_type: "Army")
-united_states.places.create(base_long: 25.7617, base_lat: -80.1918, location: "Miami", base_type: "Navy")
+united_states.places.create(base_long: 12.5029, base_lat: -12.5029, location: "Aruba", base_type: "Air Force")
+united_states.places.create(base_long: -7.9467, base_lat: -14.3559, location: "Ascension Island, Bahamas", base_type: "Air Field")
+##Australia-bases
+united_states.places.create(base_long: -12.4637, base_lat: 130.8444, location: "Darwin, Australia", base_type: "Marine")
+united_states.places.create(base_long: -23.6980, base_lat: 133.8807, location: "Alice Springs, Australia", base_type: "Satelite Surveilance Base")
+#end
+united_states.places.create(base_long: 24.7064, base_lat: -78.0195, location: "Andros Island, Bahamas", base_type: "Testing Center")
+##Bahrain-bases
+united_states.places.create(base_long: 25.9155, base_lat: 50.5853, location: "Bahrain", base_type: "Naval Support Activity")
+united_states.places.create(base_long: 26.1235, base_lat: 50.3635, location: "Bahrain", base_type: "Naval Support Activity")
+##Belgium-bases
+united_states.places.create(base_long: 50.5846, base_lat: 3.8452, location: "Hainaut, Belgium", base_type: "Air Base")
+united_states.places.create(base_long: 51.1726, base_lat: 5.4502, location: "Kleine-Brogel, Belgium", base_type: "Air Base")
+#end
+united_states.places.create(base_long: 44.5375, base_lat: 18.6735, location: "Tuzla, Bosnia", base_type: "Air Base")
+united_states.places.create(base_long: -7.3195, base_lat: 72.4229, location: "Diego Garcia, British Indian Territory", base_type: "Naval Support")
+##Bulgaria-bases
+united_states.places.create(base_long: 42.7031, base_lat: 27.2506, location: "Aytos, Bulgaria", base_type: "Air Base")
+united_states.places.create(base_long: 42.2717, base_lat: 26.2108, location: "Yambo, Bulgaria", base_type: "Air Base")
+united_states.places.create(base_long: 42.1725, base_lat: 24.4250, location: "Plovdiv, Bulgaria", base_type: "Air Base")
+united_states.places.create(base_long: 42.3010, base_lat: 27.2812, location: "Burgas, Bulgaria", base_type: "Army Training Facility")
+#end
+united_states.places.create(base_long: 12.2111, base_lat: -01.3044, location: "Ouagadougou, Burkina Faso", base_type: "Air Facility")
 
 #coordinates for Moscow
 russia = Country.create(name: "Russia", flag: "https://cdn.britannica.com/42/3842-004-F47B77BC/Flag-Russia.jpg",
@@ -16,20 +37,22 @@ russia.places.create(base_long: 40.4168, base_lat: -3.7038, location: "Madrid", 
 #coordinates for Beijing
 china = Country.create(name: "China", flag: "https://cdn.britannica.com/90/7490-004-BAD4AA72/Flag-China.jpg",
 long: 39.9042, lat: 116.4074)
-china.places.create(base_long: 47.4979, base_lat: 19.0402, location: "Budapest", base_type: "Test")
-china.places.create(base_long: 40.4168, base_lat: -3.7038, location: "Madrid", base_type: "Demo")
+china.places.create(base_long: 11.3524, base_lat: 43.3470, location: "Balbala, Djibouti", base_type: "Army Base")
+china.places.create(base_long: 14.0500, base_lat: 93.3500, location: "Coco Islands, Myanmar", base_type: "Naval SIGNIT Facility")
 
 #coordinates for Canberra
 australia = Country.create(name: "Australia", flag: "https://cdn.britannica.com/78/6078-004-77AF7322/Flag-Australia.jpg",
 long: -35.2809, lat: 149.1300)
-australia.places.create(base_long: 47.4979, base_lat: 19.0402, location: "Budapest", base_type: "Test")
-australia.places.create(base_long: 40.4168, base_lat: -3.7038, location: "Madrid", base_type: "Demo")
+australia.places.create(base_long: 5.2758, base_lat: 100.2328, location: "Penang, Malaysia", base_type: "Air Base")
+australia.places.create(base_long: 25,0137, base_lat: 55.2158, location: "Dubai, AUE", base_type: "Air Base")
 
 #coordinates for Ottawa
 canada = Country.create(name: "Canada", flag: "https://cdn.britannica.com/68/7068-004-7848FEB4/Flag-Canada.jpg",
 long: 45.4215, lat: -75.6972)
-canada.places.create(base_long: 47.4979, base_lat: 19.0402, location: "Budapest", base_type: "Test")
-canada.places.create(base_long: 40.4168, base_lat: -3.7038, location: "Madrid", base_type: "Demo")
+canada.places.create(base_long: 50.5157, base_lat: 7.0834, location: "Cologne, Germany", base_type: "Operational Support")
+canada.places.create(base_long: 18.1096, base_lat: -77.2975, location: "Jamaica", base_type: "CAF")
+canada.places.create(base_long: 29.3117, base_lat: 47.4818, location: "Kuwait", base_type: "CAF")
+canada.places.create(base_long: 14.4422, base_lat: 17.2924, location: "Dakar, Senegal", base_type: "CAF")
 
 #coordinates for Paris
 france = Country.create(name: "France", flag: "https://cdn.britannica.com/82/682-050-8AA3D6A6/Flag-France.jpg",
